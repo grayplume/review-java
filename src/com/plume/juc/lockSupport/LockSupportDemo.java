f7a3db4365412d4c29286297f3fde7ec3fc5f9ab
grayplume <birdgray@163,com>
2024-01-06 14:17:53 +0800

    LockSupport

---
+++ src/com/plume/juc/lockSupport/LockSupportDemo.java
@@ -0,0 +1,95 @@
+package com.plume.juc.lockSupport;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.LockSupport;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class LockSupportDemo {
+    public static void main(String[] args) {
+        Thread t1 = new Thread(() -> {
+            // try {
+            //     TimeUnit.SECONDS.sleep(1);
+            // } catch (InterruptedException e) {
+            //     throw new RuntimeException(e);
+            // }
+            System.out.println(Thread.currentThread().getName() + "\t ----come in");
+            LockSupport.park();
+            System.out.println(Thread.currentThread().getName() + "\t ----被唤醒");
+        }, "t1");
+        t1.start();
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        new Thread(()->{
+            LockSupport.unpark(t1);
+            System.out.println(Thread.currentThread().getName() + "\t ----发出通知");
+        },"t2").start();
+
+    }
+
+    private static void lockAwaitSignal() {
+        Lock lock = new ReentrantLock();
+        Condition condition = lock.newCondition();
+        new Thread(() -> {
+            lock.lock();
+            try {
+                System.out.println(Thread.currentThread().getName() + "\t ----come in");
+                condition.await();
+                System.out.println(Thread.currentThread().getName() + "\t ----被唤醒了");
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            } finally {
+                lock.unlock();
+            }
+        }, "t1").start();
+
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        new Thread(() -> {
+            lock.lock();
+            try {
+                condition.signal();
+                System.out.println(Thread.currentThread().getName() + "\t ----发出通知");
+            } finally {
+                lock.unlock();
+            }
+        }, "t2").start();
+    }
+
+    private static void syncWaitNotify() {
+        Object objectLock = new Object();
+
+        new Thread(() -> {
+            synchronized (objectLock) {
+                System.out.println(Thread.currentThread().getName() + "\t ----come in");
+                try {
+                    objectLock.wait();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println(Thread.currentThread().getName() + "\t ----被唤醒了");
+            }
+        }, "t1").start();
+
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        new Thread(() -> {
+            synchronized (objectLock) {
+                objectLock.notify();
+                System.out.println(Thread.currentThread().getName() + "\t ----发出通知");
+            }
+        }, "t2").start();
+    }
+}
